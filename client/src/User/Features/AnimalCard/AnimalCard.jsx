151b314f8ae0c58ab25a4aaa0b6e1b766304bb19
unknown <tomygarnero9429@gmail.com>
2022-09-28 18:56:22 -0300

    Feature/Component/Animal

--- client/src/User/Features/AnimalCard/AnimalCard.jsx
+++ client/src/User/Features/AnimalCard/AnimalCard.jsx
@@ -5,14 +5,16 @@ import ".AnimalCard.module.css";
 export default function AnimalCard({id, name, main_image, sex, size}) {
 
     return (
-        <Link to={`/home/${id}`} className="link">
+        <Link to={`/home/animal${id}`} className="link">
             <div className="animal-card">
-                <h2 className="animal-name">{`Nombre: ${name}`}</h2>
                 <div>
-                    <img className="main-img" src={main_image} alt="" width="200px" height="250px" />
+                    <h2 className="animal-name">{`Nombre: ${name}`}</h2>
+                    <div>
+                        <img className="main-img" src={main_image} alt="" width="200px" height="250px" />
+                    </div>
+                    <h3 className="animal-sex">{`Sexo: ${sex}`}</h3>
+                    <h3 className="animal-size">{`Tamaño: ${size}`}</h3>
                 </div>
-                <h3 className="animal-sex">{`Sexo: ${sex}`}</h3>
-                <h3 className="animal-size">{`Tamaño: ${size}`}</h3>
             </div>
         </Link>
     )
