14d72299b18fd2f66485dd194fb39586b08447d8
FM9408 <medina.766@hotmail.com>
2022-09-29 14:03:08 -0500

    arreglos generales

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const animal = require('./animal')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
+router.use('/animals', animal )
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
