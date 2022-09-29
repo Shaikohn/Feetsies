11573f5a3b05a4c3b4d7ff3ac668098e3444a176
unknown <tomygarnero9429@gmail.com>
2022-09-29 19:19:15 -0300

    Route AltaAdoption

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import AnimalDetails from "./User/Views/AnimalDetails/AnimalDetails.jsx";
 import ProductDetails from "./User/Views/ProductDetails/ProductDetails.jsx";
 import Adoption from "./User/Features/Form/Adoption.jsx";
+import AltaAdoption from "./User/Features/Form/AltaAdoption.jsx";
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route exact path="/home/products" element={<ProductDetails />}/>
         <Route exact path="/home/products/:id" element={<ProductDetails />}/>
         <Route exact path="/home/animals/:id" element={<AnimalDetails />}/>
         <Route exact path="/home/animals/:id/adoption" element={<Adoption />}/>
+        <Route exact path="/home/altaadoption" element={<AltaAdoption />}/>
         <Route path="*" element={<main><p>The searched route was not found</p></main>}/>
       </Routes>
     </div>
