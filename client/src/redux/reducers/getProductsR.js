2580e91fb105acfc53627143a55cc57cd5e6111d
F. Andrés Villegas <avillegas332@gmail.com>
2022-09-30 14:11:55 -0300

    complete navBar productos

--- client/src/redux/reducers/getProductsR.js
+++ client/src/redux/reducers/getProductsR.js
@@ -1,11 +1,13 @@
-import { GET_ALL_PRODUCTS } from "../actions/getProductsA"
+import { GET_ALL_PRODUCTS  } from "../actions/getProductsA"
+import {ORDER_PRODUCT_PRICE} from "../actions/orderPrice";
+import {ORDER_PRODUCT_NAME} from "../actions/orderName";
 const initialstate = {
     allProducts:[],
     allProductsCopy:[]
 }
 
-const productsReducer = (state = initialstate,{action,payload} )=>{
-    switch (action){
+const productsReducer = (state = initialstate,{type,payload} )=>{
+    switch (type){
         case GET_ALL_PRODUCTS:{
             return {
                 allProducts:payload,
@@ -13,6 +15,60 @@ const productsReducer = (state = initialstate,{action,payload} )=>{
             }
         }
 
+        case ORDER_PRODUCT_PRICE:
+            const orderedPrice = 
+                payload === "desc" ?
+                state.allProductsCopy.sort((a, b) => {
+                    if (a.price > b.price) {
+                      return -1;
+                    }
+                    if (a.price < b.price) {
+                      return 1;
+                    } else {
+                      return 0;
+                    }
+                  })
+                : [...state.allProductsCopy].sort((a, b) => {
+                    if (a.price > b.price) {
+                      return 1;
+                    }
+                    if (a.price < b.price) {
+                      return -1;
+                    } else {
+                      return 0;
+                    }
+                  })
+                return {
+                    ...state,
+                    allProductsCopy:orderedPrice
+                }
+        case ORDER_PRODUCT_NAME:
+          const orderedName = 
+                payload === "asc"
+                ? state.allProductsCopy.sort((a, b) => {
+                  if (a.name > b.name) {
+                    return 1;
+                  }
+                  if (b.name > a.name) {
+                    return -1;
+                  } else {
+                    return 0;
+                  }
+                })
+              : [...state.allProductsCopy].sort((a, b) => {
+                  if (a.name > b.name) {
+                    return -1;
+                  }
+                  if (b.name > a.name) {
+                    return 1;
+                  } else {
+                    return 0;
+                  }
+                })
+            return{
+              ...state,
+              allProductsCopy:orderedName
+            }
 
         default:
             return state
