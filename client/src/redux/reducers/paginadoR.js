293f45e871f7d0c5b9ae28844931038cdff4e9aa
unknown <tomygarnero9429@gmail.com>
2022-09-30 12:57:40 -0300

    Added Action-Reducer for SearchBar

--- client/src/redux/reducers/paginadoR.js
+++ client/src/redux/reducers/paginadoR.js
@@ -1,16 +1,26 @@
-import { SET_CURRENT_PAGE } from "../actions/paginadoA"
+import { SET_CURRENT_PAGE, RESET_PAGINATION } from "../actions/paginadoA";
+
 
 const initialSate ={
-    page:1
+    page: 1,
+    pagination: {}
 }
 
 const paginadoReducer = (state=initialSate,{action,payload})=>{
 
     switch (action){
+
         case SET_CURRENT_PAGE:
             return{
                 page:payload
             }
+
+        case RESET_PAGINATION:
+            return {
+                ...state, 
+                pagination: action.payload
+            }
+
         default:
             return state;
     }
