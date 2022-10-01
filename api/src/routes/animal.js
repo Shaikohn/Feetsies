dcb8d51d77c275a261398b04098035288549c3d2
FM9408 <medina.766@hotmail.com>
2022-10-01 17:13:28 -0500

    Ruta creada para buscar un animal con diversos factores y borrado el modelo Review

--- api/src/routes/animal.js
+++ api/src/routes/animal.js
@@ -4,11 +4,11 @@ const{getTypes} = require('../controllers/Animal_type.controller')
 const router = Router()
 
 router.get('/', getAllAnimals);
-router.post('/', createAnimal)
-router.get('/types',getTypes)
-router.get('/search', searchAnimal)
-router.get('/:id', getAnimalDetail)
-router.delete('/:id', delateAnimal)
+router.post('/', createAnimal);
+router.get('/types',getTypes);
+router.get('/search', searchAnimal);
+router.get('/:id', getAnimalDetail);
+router.delete('/:id', delateAnimal);
 
 
 module.exports = router
