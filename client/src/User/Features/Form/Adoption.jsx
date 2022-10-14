85b7cc24a70f1fcba373472020307bfd3bb65577
unknown <tomygarnero9429@gmail.com>
2022-10-14 14:50:41 -0300

    Adoption Form Styles done

--- client/src/User/Features/Form/Adoption.jsx
+++ client/src/User/Features/Form/Adoption.jsx
@@ -1,96 +1,106 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import ResponsiveAppBar from "../../Features/Header/HeaderMUI.jsx";
 import styles from "./CreateProduct.module.css";
 import { useDispatch } from "react-redux";
 import { getAllProducts } from "../../../redux/actions/getProductsA.js";
 import Swal from 'sweetalert2'
-import {
-  Grid,
-  TextField,
-  Button,
-  Card,
-  CardContent,
-  Typography,
-  Drawer,
-} from "@mui/material";
 
 import IconButton from "@mui/material/IconButton";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
 
+
+import ResponsiveAppBar from "../../Features/Header/HeaderMUI.jsx";
+
+import { Grid, Card, CardContent, Typography, TextField, Button,  Drawer} from "@mui/material";
+
+
+
 const Adoption = () => {
+
   const dispatch = useDispatch();
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({
-    defaultValues: {
-      topic: "",
-      description: "",
-      name: "",
-    },
-  });
+
+  const { register, formState: { errors }, handleSubmit } = useForm({ 
+    defaultValues: { topic: "", description: "", name: "" }});
 
   const onSubmit = (data) => console.log("Onsubmit", data);
 
-  console.log(errors);
-
   return (
-    <Drawer>
-      <div>
-        <div>
-          <ResponsiveAppBar />
-        </div>
-        <div className={styles.createForm}>
+    <div>
+      <ResponsiveAppBar />
+      <div className={styles.createForm}>
         <Grid>
-            <Card
-              style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}
-            >
+            <Card sx={{maxWidth: 450, padding: "20px 5px", margin: "0 auto"}}>
               <CardContent>
                 <Typography gutterBottom variant="h5">
                   Adoption Form
                 </Typography>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <Grid container spacing={1}>
+                    <Grid xs={12} item>
+                      <TextField
+                        error={errors.topic ? true : false}
+                        label="Topic"
+                        variant="outlined"
+                        fullWidth
+                        {...register("topic", {required: true})}
+                        aria-invalid={errors.topic ? "true" : "false"}
+                      />
+                      {errors?.topic?.type === "required" && (
+                        <span style={{ color: "red" }}>
+                          A short reason is required
+                        </span>
+                      )}
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        error={errors.name ? true : false}
+                        label="Name"
+                        variant="outlined"
+                        fullWidth
+                        {...register("name", { required: true})}
+                        aria-invalid={errors.name ? "true" : "false"}
+                      />
+                      {errors?.name?.type === "required" && (
+                        <span style={{ color: "red" }}>
+                          Your name is required
+                        </span>
+                      )}
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        error={errors.description ? true : false}
+                        label="Description"
+                        variant="outlined"
+                        multiline
+                        rows={10}
+                        fullWidth
+                        {...register("description", { required: true})}
+                        aria-invalid={errors.description ? "true" : "false"}
+                      />
+                      {errors?.description?.type === "required" && (
+                        <span style={{ color: "red" }}>
+                          You have to write a description!
+                        </span>
+                      )}
+                    </Grid>
+                    <Grid item xs={12}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      fullWidth
+                    >
+                      Adopt
+                    </Button>
+                  </Grid>
+                  </Grid>
+                </form>
               </CardContent>
             </Card>
         </Grid>
-        </div>
-        <h1></h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label>Motive</label>
-          <input
-            {...register("motive", { required: true })}
-            aria-invalid={errors.motive ? "true" : "false"}
-          />
-          {errors.motive?.type === "required" && (
-            <p role="alert">A short reason is required</p>
-          )}
-
-          <label>Name</label>
-          <input
-            {...register("name", { required: "Your name is required" })}
-            aria-invalid={errors.name ? "true" : "false"}
-          />
-          {errors.name && <p role="alert">{errors.name?.message}</p>}
-
-          <label>Description</label>
-          <textarea
-            {...register("description", {
-              required: "You have to write a description!",
-            })}
-            aria-invalid={errors.description ? "true" : "false"}
-            cols="30"
-            rows="10"
-          ></textarea>
-          {errors.description && (
-            <p role="alert">{errors.description?.message}</p>
-          )}
-
-          <button>Adopt</button>
-        </form>
       </div>
-    </Drawer>
+    </div>
   );
 };
 
