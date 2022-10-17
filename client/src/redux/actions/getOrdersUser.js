a5dc4a19448f11ca8218f2efef3be0e290b3a165
Pablo C <43583262+PCardozo@users.noreply.github.com>
2022-10-17 14:45:18 -0300

    Update getOrdersUser.js

--- client/src/redux/actions/getOrdersUser.js
+++ client/src/redux/actions/getOrdersUser.js
@@ -4,7 +4,7 @@ export const GET_PURCHASE_ORDERS_BY_USER_ID = "GET_PURCHASE_ORDERS_BY_USER_ID";
 
 export default function getPurchaseOrders (id){
     return async(dispatch)=>{
-     const orders = await axios(`http://localhost:3001/cart/getuserorders/${id}`);
+     const orders = await axios(`/cart/getuserorders/${id}`);
      return dispatch({
         type: GET_PURCHASE_ORDERS_BY_USER_ID,
         payload: orders.data
