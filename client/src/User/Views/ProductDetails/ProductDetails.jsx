11b43e9d21f1518a49a250404a42412accc9e42c
unknown <tomygarnero9429@gmail.com>
2022-10-19 17:09:26 -0300

    Styles to products Images

--- client/src/User/Views/ProductDetails/ProductDetails.jsx
+++ client/src/User/Views/ProductDetails/ProductDetails.jsx
@@ -174,7 +174,7 @@ export default function ProductDetails({ product }) {
                   my: 1.5
                 }}
                 height="80px"
-                width="auto"
+                maxWidth="auto"
               />
             ))}
           </Box>
