3b3f9c6e55c1591f1b776d432b4c65592381f90e
Anderson <andersonventosilla0@gmail.com>
2022-10-20 01:08:17 -0500

    alerts modificated

--- client/src/User/Features/AnimalCard/AnimalCard.jsx
+++ client/src/User/Features/AnimalCard/AnimalCard.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Card from "@mui/material/Card";
-import { CardActionArea } from '@mui/material';
+import { CardActionArea } from "@mui/material";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
-
+// ckdfsksdksf
 
 export default function AnimalCard({ id, name, main_image, sex, size }) {
-
   return (
-
-    <Card 
+    <Card
       elevation={5}
       key={id}
-      sx={{ 
+      sx={{
         maxWidth: 300,
         border: "1px solid #bada59",
         borderRadius: 3,
@@ -26,9 +24,9 @@ export default function AnimalCard({ id, name, main_image, sex, size }) {
         animation: "ease-in-out",
         "&:hover": {
           transform: "scale(1.02)",
-          boxShadow: "0px 0px 10px 8px #943656"
-        }
-      }} 
+          boxShadow: "0px 0px 10px 8px #943656",
+        },
+      }}
     >
       <Link to={`/home/animals/${id}`}>
         <CardActionArea>
@@ -39,33 +37,45 @@ export default function AnimalCard({ id, name, main_image, sex, size }) {
             alt="Animal Img"
           />
           <CardContent>
-            <Typography gutterBottom variant="h4" component="div" fontFamily="Segoe Print"
-              sx={{color: "#567900", textShadow: "1px 1px 5px rgb(0, 0, 0)"}}
+            <Typography
+              gutterBottom
+              variant="h4"
+              component="div"
+              fontFamily="Segoe Print"
+              sx={{ color: "#567900", textShadow: "1px 1px 5px rgb(0, 0, 0)" }}
             >
               {name}
             </Typography>
             {sex === "Female" ? (
-              <Typography variant="h5" fontFamily="Segoe Print" 
-                sx={{color: "black", textShadow: "1px 1px 5px #b93b9f"}}
+              <Typography
+                variant="h5"
+                fontFamily="Segoe Print"
+                sx={{ color: "black", textShadow: "1px 1px 5px #b93b9f" }}
               >
                 {`Sex: ${sex}`}
               </Typography>
             ) : (
-              <Typography variant="h5" fontFamily="Segoe Print" 
-                sx={{color: "black", textShadow: "1px 1px 5px #2d35ef"}}
+              <Typography
+                variant="h5"
+                fontFamily="Segoe Print"
+                sx={{ color: "black", textShadow: "1px 1px 5px #2d35ef" }}
               >
                 {`Sex: ${sex}`}
               </Typography>
             )}
             {sex === "Female" ? (
-              <Typography variant="h5" fontFamily="Segoe Print" 
-                sx={{color: "black", textShadow: "1px 1px 5px #b93b9f"}}
+              <Typography
+                variant="h5"
+                fontFamily="Segoe Print"
+                sx={{ color: "black", textShadow: "1px 1px 5px #b93b9f" }}
               >
                 {`Size: ${size}`}
               </Typography>
             ) : (
-              <Typography variant="h5" fontFamily="Segoe Print" 
-                sx={{color: "black", textShadow: "1px 1px 5px #2d35ef"}}
+              <Typography
+                variant="h5"
+                fontFamily="Segoe Print"
+                sx={{ color: "black", textShadow: "1px 1px 5px #2d35ef" }}
               >
                 {`Size: ${size}`}
               </Typography>
@@ -75,4 +85,4 @@ export default function AnimalCard({ id, name, main_image, sex, size }) {
       </Link>
     </Card>
   );
-};
+}
